d87087140aacb7f16b35f59175c624849781621f
Gabriel Mathias <gablemathias@icloud.com>
2022-10-03 23:29:11 -0300

    prereqs for conversion temp

---
+++ hackerrank/convert_temp.rb
@@ -0,0 +1,4 @@
+# temperature conversion
+# intercalates between celsius, kelvin and fahrenheit
+# input is obligatory, output default is Celsius
+# takes 3 arguments, value, input and optionally output scale
