2df25655345a563d241287ef0c057775c8eb8acd
Gabriel Mathias <gablemathias@icloud.com>
2022-12-30 20:53:59 -0300

    add log_line_parser ex

--- exercism/amusement_park.rb
+++ exercism/amusement_park.rb
@@ -19,4 +19,17 @@ class Attendee
   def revoke_pass!
     @pass_id = nil
   end
+
+  def has_pass?
+    pass_id ? true : false
+  end
+
+  def fits_ride?(ride_minimum_height)
+    height >= ride_minimum_height
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    has_pass? && fits_ride?(ride_minimum_height)
+  end
+
 end
