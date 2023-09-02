2afa365e89085a32c7aeb0fe4327982090f3e269
Gabriel Mathias <gablemathias@icloud.com>
2023-09-02 10:55:52 -0300

    port

---
+++ exercism/port_palermo.rb
@@ -0,0 +1,12 @@
+module Port
+  # TODO: define the 'IDENTIFIER' constant
+  IDENTIFIER = :PALE
+
+  def self.get_identifier(city)
+  
+  end
+
+  def self.get_terminal(ship_identifier)
+    raise 'Please implement the Port.get_terminal method'
+  end
+end
